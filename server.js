f0fbe294baa0af4bb3be24bc300415721364038b
vegascruz <cruzv@students.westerntc.edu>
2023-10-04 14:06:29 -0500

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 //this installs express library or AKA esm package
 import express from 'express';
+import connectDatabase from './config/db';
 
 //Initialize express application
 const app = express();
+
+//Connect database
+connectDatabase();
 
 //API endpoints
 app.get('/', (req, res) =>
@@ -10,4 +14,4 @@ app.get('/', (req, res) =>
 );
 
 //connection listener 
-app.listen(3000, () => console.log('Express server running on port 3000'));
+app.listen(4000, () => console.log('Express server running on port 4000'));
