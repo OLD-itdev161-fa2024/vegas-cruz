cb884acbe72117d20564ca6a164643670ad75d36
vegascruz <cruzv@students.westerntc.edu>
2023-10-05 12:25:25 -0500

    created the API endpoints for the client and server side to talk to each other

--- server.js
+++ server.js
@@ -8,10 +8,27 @@ const app = express();
 //Connect database
 connectDatabase();
 
-//API endpoints
+//Configure middleware 
+app.use(express.json({extended: false}));
+
+//API endpoints 
+/** 
+ * @route GET/
+ * @desc Test endpoint 
+ */
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 //connection listener 
 app.listen(4000, () => console.log('Express server running on port 4000'));
