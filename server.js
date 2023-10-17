1e6ba8d62a1d2aba9162ebfb7ecd870c1f55226a
vegascruz <cruzv@students.westerntc.edu>
2023-10-16 21:05:10 -0500

    Finished the activity. The server and client side are now connected, and CORS is enabled

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import {check, validationResult} from 'express-validator';
+import cors from 'cors';
 
 
 //Initialize express application
@@ -12,6 +13,11 @@ connectDatabase();
 
 //Configure middleware 
 app.use(express.json({extended: false}));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
 //API endpoints 
 /** 
@@ -49,4 +55,5 @@ app.post('/api/users',
 })
 
 //connection listener 
-app.listen(4000, () => console.log('Express server running on port 4000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
