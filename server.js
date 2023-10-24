d4cb8b57311d2d7c2c9555bbbfb4b439e54adc82
vegascruz <cruzv@students.westerntc.edu>
2023-10-23 19:34:23 -0500

    Made some changes

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/api/users',
     } else{
         return res.send(req.body);
     }
-})
+});
 
 //connection listener 
-app.listen(4000, () => console.log('Express server running on port 4000'));
+app.listen(3000, () => console.log('Express server running on port 3000'));
