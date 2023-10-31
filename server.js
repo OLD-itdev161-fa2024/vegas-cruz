50b2c421de0df4ce4c74b37efe0957dbe8239d32
vegascruz <cruzv@students.westerntc.edu>
2023-10-30 20:21:09 -0500

    completed

--- server.js
+++ server.js
@@ -78,6 +78,23 @@ async (req, res) => {
 
             //Save to the db and return
             await user.save();
+
+            //Generate and return a JWT token 
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: '10hr'},
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({token: token});
+                }
+            );
         }catch(error){
             res.status(500).send('Server error');
         }
