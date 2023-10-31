da0f78efb63e536338d21a4bd200dc011dbbeef9
vegascruz <cruzv@students.westerntc.edu>
2023-10-30 20:04:10 -0500

    Able to create and save users

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import {check, validationResult} from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 
 //Initialize express application
@@ -33,7 +35,8 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', 
+app.post(
+    '/api/users', 
 [
     check('name', 'Please enter your name')
         .not()
@@ -45,12 +48,38 @@ app.post('/api/users',
         'Please enter a password with 6 or more characters')
         .isLength({min: 6})
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(422).json({errors: errors.array()});
     } else{
-        return res.send(req.body);
+        const {name, email, password} = req.body;
+        try{
+            // check if user exists
+            let user = await User.findOne({email: email});
+            if(user){
+                return res 
+                .status(400)
+                .json({ errors: [{msg: 'User already exists'}] });
+            }
+
+            //Create a new user
+            user = new User({
+                name: name,
+                email: email,
+                password: password
+            });
+
+            //Encrypt the password 
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            //Save to the db and return
+            await user.save();
+            res.send('User successfully registered');
+        }catch(error){
+            res.status(500).send('Server error');
+        }
     }
 });
 
