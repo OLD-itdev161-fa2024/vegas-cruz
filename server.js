47c4e972d90442dd566df4f2c4e654b973a34321
vegascruz <cruzv@students.westerntc.edu>
2023-11-09 11:28:04 -0600

    Added the get posts, get post by id and delete post by id endpoints.. sorry for the long commit, forgot to earlier

--- server.js
+++ server.js
@@ -171,10 +171,10 @@ app.post(
     [
         auth,
         [
-            check('title', 'Title text is required')
+            check("title", "Title text is required")
                 .not()
                 .isEmpty(),
-            check('body', 'Body text is required')
+            check("body", "Body text is required")
                 .not()
                 .isEmpty()
         ]
@@ -207,6 +207,68 @@ app.post(
         }
     }
 );
+
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+app.get('/api/posts', auth, async(req, res) => {
+    try{
+        const posts = await Post.find().sort({date: -1});
+
+        res.json(posts);
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route GET api/posts:id
+ * @desc GET post
+ */
+app.get('/api/posts:id', auth, async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+
+        //Make sure the post was found
+        if(!post){
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        res.json(post);
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc DELETE a post
+ */
+app.delete('/api/posts/:id', auth, async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+
+        //Makre sure the post was found
+        if(!post){
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        //Make sure the request user created the post
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg: 'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({msg: 'Post removed'});
+    } catch(error){
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
 
 //connection listener 
 const port = 5000;
